fee16607bac58914b5c99ffd936ffad54040b41a
abmott <adam.mott@csaa.com>
2017-06-07 15:19:07 -0700

    changed zapier.sh to install curl

--- pcf-cfops-backup/bin/push-zapier.sh
+++ pcf-cfops-backup/bin/push-zapier.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -ue
+apt-get install curl -y
 #curl -H "Content-Type: application/json" -X POST -d '{"access_token":"'"$access_token"'","environment":"'"$environment"'", #"local_username":"'"$local_username"'", "title": "'"$title"'", "text":"'"$text"'", "alert_type": "'"$alert_type"'", "revision":"'"$revision"'", "success": #"'"$success"'", "newrelic_app_url": "'"$newrelic_app_url"'", "newrelic_api_key": "'"$newrelic_api_key"'", "push_newrelic": "'"$push_newrelic"'"}' $url
 #else
 curl -H "Content-Type: application/json" -X POST -d '{"Name": "Adam Mott"}' $url
