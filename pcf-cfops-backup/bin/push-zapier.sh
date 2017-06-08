f87adc73226e0f784d35b285273aaeda37059836
abmott <adam.mott@csaa.com>
2017-06-08 14:58:56 -0700

    modifited zapier to work on all three environments

--- pcf-cfops-backup/bin/push-zapier.sh
+++ pcf-cfops-backup/bin/push-zapier.sh
@@ -3,5 +3,5 @@ set -ue
 apt-get update -y && apt-get install curl -y
 #curl -H "Content-Type: application/json" -X POST -d '{"access_token":"'"$access_token"'","environment":"'"$environment"'", #"local_username":"'"$local_username"'", "title": "'"$title"'", "text":"'"$text"'", "alert_type": "'"$alert_type"'", "revision":"'"$revision"'", "success": #"'"$success"'", "newrelic_app_url": "'"$newrelic_app_url"'", "newrelic_api_key": "'"$newrelic_api_key"'", "push_newrelic": "'"$push_newrelic"'"}' $url
 #else
-curl -H "Content-Type: application/json" -X POST -d '{"Name": "Adam Mott"}' $url
+curl -H "Content-Type: application/json" -X POST -d '{"name":"'"$pcf_env_backup"'","product":"'"$backup_product"'","passfail":"'"$passfail"'"}' $url
 #fi
