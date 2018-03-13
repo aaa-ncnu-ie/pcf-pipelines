b1667fc388805cf181b2b6ac889595cc5eec25d7
abmott <adam.mott@csaa.com>
2018-03-13 09:57:13 -0700

    changed old ospman power off

--- tasks/deploy-opsman-vm/task.sh
+++ tasks/deploy-opsman-vm/task.sh
@@ -65,9 +65,9 @@ EOF
   govc vm.change -c=2 -vm="${opsman_name}"
 
   echo "Shutting down OLD OpsMgr VM... ${OPSMAN_IP}"
-  old_opsman_path="$(govc find "${GOVC_RESOURCE_POOL}" -type m -guest.ipAddress "${OPSMAN_IP}" -runtime.powerState poweredOn)"
-  govc device.disconnect -vm.ipath="${old_opsman_path}" ethernet-0
-  govc vm.power -off=true -vm.ipath="${old_opsman_path}"
+  #old_opsman_path="$(govc find "${GOVC_RESOURCE_POOL}" -type m -guest.ipAddress "${OPSMAN_IP}" -runtime.powerState poweredOn)"
+  #govc device.disconnect -vm.ipath="${old_opsman_path}" ethernet-0
+  govc vm.power -off=true -vm.ip="${OPSMAN_IP}"
 
   echo "Starting OpsMgr VM... /${GOVC_DATACENTER}/${OPSMAN_VM_FOLDER}/${opsman_name}"
   govc vm.power -on=true "${opsman_name}"
